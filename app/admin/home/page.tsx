0ba360db81f7a0f35fca480f22a732d9b7e0b258
Raihanah33rpl <raihanahnafidhiasonyata@gmail.com>
2026-05-09 23:35:48 +0700

    update fitur register

---
+++ app/admin/home/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <div className="flex justify-center bg-whitew-full h-dvh items-center">
+      <h1 className="text-3xl font-bold text-center mb-4">Dashboard</h1>
+      
+    </div>
+  );
+};
+export default DashboardPage;
